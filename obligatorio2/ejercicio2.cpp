01a8ce51e3e85f9e5e0e93cfce1b84f6ae5b8228
jesusluque01 <jesusluque@correo.ugr.es>
2022-04-05 17:57:38 +0200

    de locos

---
+++ obligatorio2/ejercicio2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+#include <fstream>
+
+using namespace std;
+//para incluir string debe estar antes using namespace
+#include <string>
+int main (void)
+{
+    
+}
